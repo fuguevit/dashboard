87945755f15f3f75ea0983299378065efddcadd5
wangxuan <wangxuan@qgpic.com>
2016-12-27 14:39:24 +0800

    bug fix

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,16 +3,23 @@ import './bootstrap.js';
 import Echo from 'laravel-echo';
 import Vue from 'vue';
 
-import CurrentTime from './components/CurrentTime.vue';
+import CurrentTime from './components/CurrentTime';
+import GithubFile from './components/GithubFile';
+import GoogleCalendar from './components/GoogleCalendar';
 
 new Vue({
     e1: '#dashboard',
 
     components: {
-        CurrentTime
+        CurrentTime,
+        GithubFile,
+        GoogleCalendar,
     },
     
     created() {
-        
+        this.echo = new Echo({
+            boradcaster: 'pusher',
+            key: window.dashboard.pusherKey,
+        });
     },
 });
