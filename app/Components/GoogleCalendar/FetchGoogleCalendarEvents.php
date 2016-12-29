040fd91df91b71c3293aa6500a1d315db3f7cefb
wangxuan <wangxuan@qgpic.com>
2016-12-29 12:54:30 +0800

    implement command eventsfetched

--- app/Components/GoogleCalendar/FetchGoogleCalendarEvents.php
+++ app/Components/GoogleCalendar/FetchGoogleCalendarEvents.php
@@ -2,3 +2,40 @@
 
 namespace App\Components\GoogleCalendar;
 
+use App\Events\GoogleCalendar\EventsFetched;
+use Carbon\Carbon;
+use DateTime;
+use Illuminate\Console\Command;
+use Spatie\GoogleCalendar\Event;
+
+class FetchGoogleCalendarEvents extends Command
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $signature = 'dashboard:calendar';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Fetch Google Calendar events.';
+
+    public function handle()
+    {
+        $events = collect(Event::get())
+            ->map(function(Event $event) {
+                return [
+                    'name'  => $event->name,
+                    'date'  => Carbon::createFromFormat('Y-m-d H:i:s', $event->getSortDate())->format(DateTime::ATOM)
+                ];
+            })
+            ->unique('name')
+            ->toArray();
+        
+        event(new EventsFetched($events));
+    }
+}
--- app/Events/GoogleCalendar/EventsFetched.php
+++ app/Events/GoogleCalendar/EventsFetched.php
@@ -1,3 +1,18 @@
 <?php
 
 namespace App\Events\GoogleCalendar;
+
+use App\Events\DashboardEvent;
+
+class EventsFetched extends DashboardEvent
+{
+    /**
+     * @var array
+     */
+    public $event;
+    
+    public function __construct(array $events)
+    {
+        $this->events = $events;
+    }
+}
