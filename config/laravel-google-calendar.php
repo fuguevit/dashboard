b67bce674e77b7b5a04e30b59d7cd7d90105e3e4
wangxuan <wangxuan@qgpic.com>
2016-12-29 18:44:36 +0800

    update configuration

---
+++ config/laravel-google-calendar.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    /*
+     * Path to a json file containing the credentials of a Google Service account.
+     */
+    'client_secret_json' => storage_path('app/laravel-google-calendar/client_secret.json'),
+
+    /*
+     *  The id of the Google Calendar that will be used by default.
+     */
+    'calendar_id' => '7042e51c97f7bcbf2ff0c322890bfa95f0592bfc',
+
+];
