9b23f0be82968369be72d3e1cacf51ee0289398b
wangxuan <wangxuan@qgpic.com>
2017-01-05 19:03:59 +0800

    minor changed

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -35,6 +35,9 @@ return [
             'key' => env('PUSHER_KEY'),
             'secret' => env('PUSHER_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
+            'options' => [
+                'cluster' => env('PUSHER_CLUSTER') // require: pusher/pusher-php-server: ~2.4.1
+            ],
         ],
 
         'redis' => [
