2fc48943f885b3a53809a38dee4da67646c5a9a5
wangxuan <wangxuan@qgpic.com>
2017-01-09 19:13:50 +0800

    bumped

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,6 +4,7 @@
     @javascript(compact('pusherKey'))
     <div class="dashboard" id="dashboard">
         <google-calendar grid="a1:a2"></google-calendar>
+        <last-fm grid="b1:c1"></last-fm>
         <current-time grid="d1" dateformat="ddd DD/MM"></current-time>
         <internet-connection grid="d2"></internet-connection>
     </div>
